4f6583ffb304b1685b56616c5140a4947d838c32
Mohammad Waseem Akhtar <mailto.waseem@gmail.com>
2016-06-19 20:37:44 +0530

    Adding python programs

---
+++ py_loops.py
@@ -0,0 +1,18 @@
+#Declare a list x
+x=[1,2,13,4,15]
+print('printing the list declared:', x)
+
+#Iterate the list forward and print the value
+for i in x:
+    print(i)
+
+#Length of the list
+print('length of the list is:', len(x)) 
+
+#Print the new line
+print("")
+
+#Decrement the value held in i until it become zero
+while(i):
+    i = i-1
+    print(i)
